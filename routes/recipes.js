3aa847c9b2dbfb55c578cde24081ef0a0c3e487f
leah mashi <leahma@post.bgu.ac.il>
2022-06-10 20:38:03 +0300

    add get random recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,15 +6,33 @@ router.get("/", (req, res) => res.send("im here"));
 
 
 /**
+ * This path returns 3 random preview recipes
+ */
+ router.get("/random", async (req, res, next) => {
+  try {
+    let random_3_recipes = await recipes_utils.getThreeRandomRecipes();
+    res.send(random_3_recipes);
+  } catch (error) {
+    next(error);
+  }
+})
+
+
+/**
  * This path returns a full details of a recipe by its id
  */
 router.get("/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    // TODO - show if user viewed the recipe
+    // TODO - show if in favorites
+
     res.send(recipe);
   } catch (error) {
     next(error);
   }
 });
+
+
 
 module.exports = router;
