b079993140daf16085c06e64c76c73202d0c9114
shahar <gefensha@post.bgu.ac.il>
2022-06-12 10:22:42 +0300

    fixed exist check

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
+const DButils = require("../routes/utils/DButils");
 
 router.get("/", (req, res) => res.send("im here"));
 
@@ -33,20 +34,26 @@ router.get("/:recipeId", async (req, res, next) => {
     console.log(viewedSecond)
     console.log(viewedThird)
     if(viewedFirst.length == 0){
+      console.log("clear")
       await recipes_utils.addRecipe(user_id,recipe);
     }
     else if(viewedSecond.length==0){
+      console.log("only one in")
       if(recipe.id!=viewedFirst[0].recipeID){
+        console.log("new one")
         await recipes_utils.addRecipe(user_id,recipe);
         recipes_utils.setPlace(viewedFirst[0],2)
       }
     }
     else if(viewedThird.length==0){
+      console.log("only two in")
       if(recipe.id==viewedSecond[0].recipeID){
+        console.log("change the first  and second")
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedSecond[0],1)
       }
       else if((recipe.id!=viewedSecond[0].recipeID)&&(recipe.id!=viewedFirst[0].recipeID)){
+        console.log("new one")
         await recipes_utils.addRecipe(user_id,recipe);
         recipes_utils.setPlace(viewedSecond[0],3)
         recipes_utils.setPlace(viewedFirst[0],2)
@@ -55,18 +62,20 @@ router.get("/:recipeId", async (req, res, next) => {
 
     else{
 
-
+      console.log("more then 3")
       if((recipe.id!=viewedFirst[0].recipeID) && (recipe.id!=viewedSecond[0].recipeID) && (recipe.id!=viewedThird[0].recipeID)){
-        var exist=recipes_utils.exist(recipe)
+        var exist=await recipes_utils.exist(recipe)
+        console.log("the new isnt in the 3")
         console.log(exist)
         if(exist.length>0){
+          console.log("the new isnt new")
           recipes_utils.setPlace(viewedSecond[0],3)
           recipes_utils.setPlace(viewedFirst[0],2)
           recipes_utils.setPlace(exist[0],1);
           recipes_utils.setPlace(viewedThird[0],0)
         }
         else{
-          console.log(viewedFirst)
+          console.log("the new is new")
           await recipes_utils.addRecipe(user_id,recipe);
           recipes_utils.setPlace(viewedSecond[0],3)
           recipes_utils.setPlace(viewedFirst[0],2)
@@ -74,18 +83,22 @@ router.get("/:recipeId", async (req, res, next) => {
         }
       }
       else if(recipe.id==viewedSecond[0].recipeID){
+        console.log("the new is 2")
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedSecond[0],1)
       }
       else if(recipe.id==viewedThird[0].recipeID){
+        console.log("the new is 3")
         recipes_utils.setPlace(viewedSecond[0],3)
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedThird[0],1)
       }
+
   }
     
 
     // TODO - show if in favorites
+    console.log(await DButils.execQuery(`SELECT * FROM recipes`))
 
     res.send(recipe);
   } catch (error) {
