19b3e1fab3081d9763b628fc78d3810b02fd8c93
shahar <gefensha@post.bgu.ac.il>
2022-06-12 10:29:56 +0300

    comments

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
-const DButils = require("../routes/utils/DButils");
 
 router.get("/", (req, res) => res.send("im here"));
 
@@ -26,69 +25,69 @@ router.get("/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
     // TODO - show if user viewed the recipe
+
+    //What user_id is current in
     const user_id = 1;
+    //The current last 3 seen
     var viewedFirst=await recipes_utils.getPlace(1);
     var viewedSecond=await recipes_utils.getPlace(2);
     var viewedThird=await recipes_utils.getPlace(3);
-    console.log(viewedFirst)
-    console.log(viewedSecond)
-    console.log(viewedThird)
+    //If we clicked the first time
     if(viewedFirst.length == 0){
-      console.log("clear")
       await recipes_utils.addRecipe(user_id,recipe);
     }
+
+    //If we clicked the second time
     else if(viewedSecond.length==0){
-      console.log("only one in")
+      //If we dont watch the last one seen
       if(recipe.id!=viewedFirst[0].recipeID){
-        console.log("new one")
         await recipes_utils.addRecipe(user_id,recipe);
         recipes_utils.setPlace(viewedFirst[0],2)
       }
     }
+
+    //If we clicked the third time
     else if(viewedThird.length==0){
-      console.log("only two in")
+      //If we watch the second one we need to swap between first and second
       if(recipe.id==viewedSecond[0].recipeID){
-        console.log("change the first  and second")
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedSecond[0],1)
       }
+      //If we watch new recipe
       else if((recipe.id!=viewedSecond[0].recipeID)&&(recipe.id!=viewedFirst[0].recipeID)){
-        console.log("new one")
         await recipes_utils.addRecipe(user_id,recipe);
         recipes_utils.setPlace(viewedSecond[0],3)
         recipes_utils.setPlace(viewedFirst[0],2)
       }
     }
 
+    //If we watch more then three already
     else{
-
-      console.log("more then 3")
+      //check if the new recipe isnt in the last 3 seen
       if((recipe.id!=viewedFirst[0].recipeID) && (recipe.id!=viewedSecond[0].recipeID) && (recipe.id!=viewedThird[0].recipeID)){
         var exist=await recipes_utils.exist(recipe)
-        console.log("the new isnt in the 3")
-        console.log(exist)
+        //check if recipe in the DataBase
         if(exist.length>0){
-          console.log("the new isnt new")
           recipes_utils.setPlace(viewedSecond[0],3)
           recipes_utils.setPlace(viewedFirst[0],2)
           recipes_utils.setPlace(exist[0],1);
           recipes_utils.setPlace(viewedThird[0],0)
         }
+        //the recipe isnt in the DataBase
         else{
-          console.log("the new is new")
           await recipes_utils.addRecipe(user_id,recipe);
           recipes_utils.setPlace(viewedSecond[0],3)
           recipes_utils.setPlace(viewedFirst[0],2)
           recipes_utils.setPlace(viewedThird[0],0)
         }
       }
+      //if the recipe is in second last seen need to swap first and second
       else if(recipe.id==viewedSecond[0].recipeID){
-        console.log("the new is 2")
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedSecond[0],1)
       }
+      //if the recipe is 3 last seen
       else if(recipe.id==viewedThird[0].recipeID){
-        console.log("the new is 3")
         recipes_utils.setPlace(viewedSecond[0],3)
         recipes_utils.setPlace(viewedFirst[0],2)
         recipes_utils.setPlace(viewedThird[0],1)
@@ -98,7 +97,6 @@ router.get("/:recipeId", async (req, res, next) => {
     
 
     // TODO - show if in favorites
-    console.log(await DButils.execQuery(`SELECT * FROM recipes`))
 
     res.send(recipe);
   } catch (error) {
